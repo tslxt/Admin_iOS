9e702c878d7f0db87f963b3027b1be4ea4031393
lxt <15460854@qq.com>
2017-11-20 15:16:08 +0800

    init

---
+++ Admin_iOS/Admin_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Admin_iOS
+//
+//  Created by LxT on 2017/11/20.
+//  Copyright © 2017年 LxT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
